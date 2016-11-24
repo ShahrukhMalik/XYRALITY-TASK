b8ab8bf2e0dd2f5ad1cf61bf7389aff99f980d0d
Shahrukh <srmalik666@gmail.com>
2016-11-24 17:29:40 +0100

    Initial commit

---
+++ XYRALITY TASK/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  XYRALITY TASK
+//
+//  Created by Shahrukh on 24/11/2016.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+#pragma mark -
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ XYRALITY TASK/Classes/View Controllers/XTLoginViewController.m
@@ -0,0 +1,33 @@
+//
+//  XTLoginViewController.m
+//  XYRALITY TASK
+//
+//  Created by Shahrukh on 24/11/2016.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+#import "XTLoginViewController.h"
+
+
+#pragma mark -
+
+@interface XTLoginViewController ()
+
+@end
+
+
+#pragma mark -
+
+@implementation XTLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
